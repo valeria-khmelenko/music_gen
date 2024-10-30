923081b5e830fc2947ea5488ac231ffff9559593
valeria-khmelenko <109990431+valeria-khmelenko@users.noreply.github.com>
2024-10-30 19:53:29 +0300

    music_load.py

--- music_load.py
+++ music_load.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 
-audio_file = open("/blob/main/music/upload.wav", "rb")
+audio_file = open("/blob/main/music/upload.mp3", "rb")
 audio_bytes = audio_file.read()
 
-st.audio(audio_bytes, format="audio/wav")
+st.audio(audio_bytes, format="audio/mpeg")
