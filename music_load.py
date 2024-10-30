83ade9d11a4a5900079e582cae9667ec88a414d1
valeria-khmelenko <109990431+valeria-khmelenko@users.noreply.github.com>
2024-10-30 17:39:25 +0300

    music_load.py

--- music_load.py
+++ music_load.py
@@ -1,19 +1,4 @@
 import streamlit as st
-import librosa
-import numpy as np
-import soundfile as sf
 
-AUDIO_FILE = '/music/upload.wav'
-
-def play_audio(audio_file):
-
-    data, sr = librosa.load(audio_file)
-    st.audio(data, sample_rate=sr) 
-
-def main():
-    st.title("Simple Audio Player")
-
-    play_audio(AUDIO_FILE)
-
-if __name__ == "__main__":
-    main()
+st.audio("/music/upload.wav", format="audio/wav", loop=True)
+    
